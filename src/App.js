b312fd99ce02c6f1e1a2856a2e9b4a57f450b42a
cw-barry <barry@clarusway.com>
2022-10-11 18:57:25 -0400

    update data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
-import data from './data';
+import dataFromFile from './data';
 import List from './List';
 function App() {
-  const [people, setPeople] = useState(data);
+  const [data, setData] = useState(dataFromFile);
+  const [people, setPeople] = useState(data.slice(0, 5));
+  const [index, setIndex] = useState(0);
   return (
     <main>
       <section className="container">
         <h3>Employee List</h3>
         <h5>({people.length} Employees)</h5>
         <List people={people} />
+        <div>
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
       </section>
     </main>
   );
--- src/List.js
+++ src/List.js
@@ -4,14 +4,15 @@ const List = ({ people }) => {
   return (
     <>
       {people.map((person) => {
-        const {
-          id: { value: id },
-          name: { first, last },
-          dob: { age },
-          picture: { large: image },
-          email,
-        } = person;
-        const name = first + ' ' + last;
+        // const {
+        //   id: { value: id },
+        //   name: { first, last },
+        //   dob: { age },
+        //   picture: { large: image },
+        //   email,
+        // } = person;
+        // const name = first + ' ' + last;
+        const { id, image, name, email, age } = person;
         return (
           <article key={id} className="person">
             <img src={image} alt={name} />
